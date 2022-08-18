17b8b0e5ccd0b42bd4f00f03a8618ae9b0894599
Ray <raysam.gas@gmail.com>
2022-08-18 20:28:49 +0700

    Simple msg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>Thanks</p>
+      <p>
+        Thank you so much for visiting my simple web, i plan to make it into a
+        portfolio interactive website, but rn it is still super simple and
+        looks... meh you know i'm not satisfied, but i'm working on it 😉
+      </p>
     </footer>
   );
 };
