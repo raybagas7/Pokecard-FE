bef709925c3d58d5b7f2d4cb21cefd56b5f6e81f
raybagas7 <raysam.gas@gmail.com>
2023-10-26 23:05:14 +0700

    fix(BASE_URL): into proper address with https and pomeoncard.lol domain for API

--- src/utils/network-data.js
+++ src/utils/network-data.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://188.166.183.186:5000';
+const BASE_URL = 'https://www.pokemoncard.lol';
 //http://localhost:5000
 //https://www.pokemoncard.lol
 const getAccessToken = () => {
